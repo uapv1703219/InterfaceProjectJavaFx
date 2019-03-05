2152b5b85d67d80069b0f429fdbf98366d9af2de
Gerard Florian <florian.gerard2@alumni.univ-avignon.fr>
2019-03-05 11:24:20 +0100

    Ajout d'un cercle qui fait plip et plop, et red and blue

--- src/main/test.java
+++ src/main/test.java
@@ -1,35 +1,78 @@
 package main;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 
 	public class test extends Application {
 	    
+		static boolean hide;
+		static boolean color;
+		
 		public static void main(String[] args) {
 	        launch(args);
 	    }
 	    
 	    @Override
-	    public void start(Stage primaryStage) {
-	        primaryStage.setTitle("Hello World!");
-	        Button btn = new Button();
-	        btn.setText("Say 'Hello World'");
-	        btn.setOnAction(new EventHandler<ActionEvent>() {
+	    public void start(Stage stage) throws IOException {
+	    	try
+	    	{
+	    	Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+	        stage.setTitle("FXML Welcome");
+	        stage.setScene(new Scene(root));
+	        stage.show();
+	        }
+	    	catch(IOException e)
+	    	{
+	    		e.printStackTrace();
+	    	}
 	 
-	            @Override
-	            public void handle(ActionEvent event) {
-	                System.out.println("Hello World!");
-	            }
-	        });
-	        
-	        StackPane root = new StackPane();
-	        root.getChildren().add(btn);
-	        primaryStage.setScene(new Scene(root, 300, 250));
-	        primaryStage.show();
+	    }
+	    
+	    public HBox createHBox () {       
+        	HBox hbox = new HBox();      
+        	hbox.setStyle("−fx−background−color :#336699;");
+        	Button b1 = new Button("Bouton1");      
+        	Button b2 = new Button("Bouton2");  
+        	Button b3 = new Button("Bouton3");
+        	hbox.getChildren().addAll(b1, b2, b3); 
+        	return hbox; 
+        	}
+	    
+	    public void handle(ActionEvent event) {
+            System.out.println("Hello World!");
+        }
+	    
+	    public void hideCircle(ActionEvent event) {
+	    	Circle circle = (Circle) ( ((Node) event.getSource()).getScene().lookup("#bluecircle"));
+	    	circle.setVisible(hide);
+	    	hide = !hide;
+	    }
+	    
+	    public void changeColorCircle(ActionEvent event) {
+	    	Circle circle = (Circle) ( ((Node) event.getSource()).getScene().lookup("#bluecircle"));
+	    	if(color)
+	    	{
+	    		circle.setFill(Color.RED);
+	    	}
+	    	else
+	    	{
+	    		circle.setFill(Color.BLUE);
+	    	}
+	    	color = !color;
 	    }
 }
